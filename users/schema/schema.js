6e0f354eb8f0b246b924f5e9c25e5a98baf6c1cb
Prateek Varma <prtk.varma@gmail.com>
2022-08-02 17:32:44 -0600

    Created another 'type' of GraphQL data called RootQuery, and hardcoded some dummy user data

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,12 +1,35 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
 
+//hardcoded data with 2 users
+const users = [
+  { id: "23", firstName: "Bill", age: 20 },
+  { id: "47", firstName: "Samantha", age: 21 },
+];
+
+//Below is a "Type" of data called UserType
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+  },
+});
+
+//Below, "Type" of data called RootQuery
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        //this is the function that goes into the backend, and gets the data
+        return _.find(users, { id: args.id })
+      },
+    },
   },
 });
